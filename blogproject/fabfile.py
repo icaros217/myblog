72f0d64f6c1a799f0e7e104d1412bae689ffbcd6
chw217 <chw217@163.com>
2017-07-22 01:10:12 +0800

    ai

--- blogproject/fabfile.py
+++ blogproject/fabfile.py
@@ -15,14 +15,14 @@ env.port = '22'
 
 
 def deploy():
-    source_folder = '/home/chw/sites/chenghongwei.com/myblog' 
+    source_folder = '/home/chw/sites/chenghongwei.com/myblog/blogproject' 
 
-    run('cd %s && git pull' % source_folder) 
+    run('cd %s && git reset --hard && git pull' % source_folder) 
     run("""
         cd {} &&
-        ../env/bin/pip install -r requirements.txt &&
-        ../env/bin/python3 manage.py collectstatic --noinput &&
-        ../env/bin/python3 manage.py migrate
+        ../../env/bin/pip install -r requirements.txt &&
+        ../../env/bin/python3 manage.py collectstatic --noinput &&
+        ../../env/bin/python3 manage.py migrate
         """.format(source_folder)) 
     sudo('restart gunicorn-chenghongwei.com') 
     sudo('service nginx reload')
