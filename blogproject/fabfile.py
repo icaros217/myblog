c4a3329314436869245ad46d5937098452c7528a
chw217 <chw217@163.com>
2017-07-21 21:09:51 +0800

    l

--- blogproject/fabfile.py
+++ blogproject/fabfile.py
@@ -24,5 +24,5 @@ def deploy():
         ../../env/bin/python3 manage.py collectstatic --noinput &&
         ../../env/bin/python3 manage.py migrate
         """.format(source_folder)) 
-    sudo('start gunicorn-chenghongwei.com') 
+    sudo('restart gunicorn-chenghongwei.com') 
     sudo('service nginx reload')
