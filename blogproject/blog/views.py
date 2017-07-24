efd884de9691391b01dc4784082853e8c1c6d84a
chw217 <chw217@163.com>
2017-07-24 22:52:19 +0800

    t2

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -229,7 +229,7 @@ class PostDetailView(DetailView):
     def get_object(self, queryset=None):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
-         md = markdown.Markdown(extensions=[
+        md = markdown.Markdown(extensions=[
             'markdown.extensions.extra',
             'markdown.extensions.codehilite',
             TocExtension(slugify=slugify),
